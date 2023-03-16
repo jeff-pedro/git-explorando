188672145cbda8f3e925a4f1b47b6b889e53998a
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-03-16 12:40:27 -0300

    alterando o app com mais um console log

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("Aplicação está se conectando no banco de dados")
+// mais um console log de teste
+console.log("Foi feito mais um commit")
