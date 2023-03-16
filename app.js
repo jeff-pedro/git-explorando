7a21b683ef30180248b61360379810b77d1067b6
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-03-16 18:41:14 -0300

    voltando alteraçoes

--- app.js
+++ app.js
@@ -1,4 +1,1 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("Aplicação está se conectando no banco de dados")
-// mais um console log de teste
-console.log("Foi feito mais um commit")
